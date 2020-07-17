720c0c7ce7cf14d69f82d3fd248f585354a6759a
Ayako Tanaka <ayakosomethingxxx@gmail.com>
2020-07-17 16:56:58 +0900

    topページとnotfoundページの作成

---
+++ src/Notfound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="not-found">
+    <p>お探しのページは見つかりません</p>
+    <router-link to="/">Topページへ行く</router-link>
+  </div>
+</template>
---
+++ src/Top.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="top">
+    <h1>Ayako Tanaka</h1>
+    <p>~Portfolio site~</p>
+    <h1>under construction</h1>
+  </div>
+</template>
