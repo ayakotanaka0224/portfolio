93841c9b35dfd902f7c144fdf2f3ae6551cbbbd0
Ayako Tanaka <ayakosomethingxxx@gmail.com>
2020-08-28 17:24:25 +0900

    sccsのやり方で記述

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,15 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: "Quicksand", "M PLUS Rounded 1c", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#menu-list {
-  position: absolute;
-  top: 0;
+  #menu-list {
+    top: 0;
+  }
 }
 </style>
